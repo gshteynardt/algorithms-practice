4a0f584e9d84b85011161282312fe352fd128679
German Shtey <gvshteynardt@gmail.com>
2022-06-03 20:13:28 +0300

    added solution Find All Numbers Disappeared in an Array

---
+++ problems/mergeSortedArray/index.js
@@ -0,0 +1,23 @@
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = (m + n) -1;
+
+  while(i >=0 && j >=0) {
+    if(nums1[i] < nums2[j]) {
+      nums1[k] = nums2[j];
+      j--;
+      k--;
+    } else {
+      nums1[k] = nums1[i];
+      i--;
+      k--;
+    }
+  }
+
+  while(j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+  }
+};
